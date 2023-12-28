e09c237895d859422d3c2e1f87528ed6eb412350
Hy-Iam-Noval <upangwoy@gmail.com>
2023-12-29 06:37:44 +0700

    w

--- main.ts
+++ main.ts
@@ -10,7 +10,7 @@ app.use(express.json())
 app.use(cors({origin:process.env.ORIGIN, methods:"POST"}))
 
 app.post("/add", (req, res)=>{
-   res.json(req.body.data)
+   res.json({data: req.body.data})
 })
 
 app.listen("8000", ()=>console.log("runing"))
